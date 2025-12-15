f1b1b82b7c307937abe671cfbee0307d80f50977
moi.rodriguez <moi.rodriguez@duocuc.cl>
2025-12-15 12:06:25 -0300

    Refactor: Diseño de la pantalla de compra exitosa
    Se ha rediseñado la pantalla `OrderSuccessScreen` para mejorar la experiencia de usuario después de una compra.

--- app/src/main/java/com/example/perfulandia/ui/screens/OrderSuccessScreen.kt
+++ app/src/main/java/com/example/perfulandia/ui/screens/OrderSuccessScreen.kt
@@ -1,19 +1,51 @@
 package com.example.perfulandia.ui.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.perfulandia.ui.navigation.Screen
 
 @Composable
 fun OrderSuccessScreen(navController: NavController) {
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Hola Mundo: ¡Compra Exitosa!")
+        Icon(
+            imageVector = Icons.Default.CheckCircle,
+            contentDescription = "Compra Exitosa",
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(120.dp)
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = "¡Compra Exitosa!",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(48.dp))
+        Button(onClick = {
+            navController.popBackStack(Screen.Home.route, inclusive = false)
+            navController.navigate(Screen.Home.route) {
+                popUpTo(Screen.Home.route) { inclusive = true }
+                launchSingleTop = true
+            }
+        }) {
+            Text("Volver al Inicio")
+        }
     }
 }
